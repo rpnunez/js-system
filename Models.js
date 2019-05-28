0fa8ec9e62abf4d9ca3913ed27f9ca83f37f7ad6
Raymond Nunez <rnunez@belusallc.com>
2019-05-27 20:20:06 -0400

    Actual initial commit

---
+++ Models.js
@@ -0,0 +1,16 @@
+System.Models = function(){};
+
+System.Models.Base = function(properties) {
+    var options = {
+
+    }
+};
+
+ Person.prototype.getName = function() {
+     return this.name;
+ };
+
+ Person.prototype.setName = function(name = '') {
+     this.name = name || '';
+ }
+
---
+++ System.js
@@ -0,0 +1,5 @@
+var System;
+
+System.prototype.getOS = function() {
+
+};
