706a3dde70cdddc9caaa602795b63e82831b8c44
Raymond Nunez <rnunez@belusallc.com>
2019-06-15 22:27:54 -0400

    -console.log, Lowercased model props, var to let

--- Models.js
+++ Models.js
@@ -7,12 +7,12 @@ System.Models = function(){};
  */
 System.Models.Base = function(ModelDefaults, ModelOptions, Callback = null) {
     // Reference to Base
-    var _this = this;
+    let _this = this;
 
     // Properties shared across all Models
-    var BaseDefaults = {
-        _UUID: System.Helpers.Common.generateUUID(),
-        _Callback: Callback != null ? Callback : null
+    let BaseDefaults = {
+        _uuid: System.Helpers.Common.generateUUID(),
+        _callback: Callback != null ? Callback : null
     };
 
     // Model constructor
@@ -22,11 +22,8 @@ System.Models.Base = function(ModelDefaults, ModelOptions, Callback = null) {
         // Replace and/or add properties from our model options
         _.extend(BaseDefaults, ModelOptions);
 
-        console.log('#System.Models.Base: BaseDefaults: ', BaseDefaults);
-
-
         // Make each property in our base defaults into a property into this model
-        for (var prop in BaseDefaults) { 
+        for (let prop in BaseDefaults) { 
             if (BaseDefaults.hasOwnProperty(prop)) {
                 //_this[prop] = BaseDefaults[prop];
 
@@ -36,38 +33,29 @@ System.Models.Base = function(ModelDefaults, ModelOptions, Callback = null) {
                     configurable: true,
                     enumerable: true
                 });
-
-                console.log('#System.Models.Base: added property '+ prop +' to model ', _this);
             }
         }
     };
 
-    console.log('#System.Models.Base: Copy of current model: ', BaseDefaults);
-
     // Fire constructor
     _this._construct();
 
-    var _model = _.clone(_this);
-
-    // Fire callback if it is truthy
-    if (_this._Callback != null) {
-        console.log('#System.Models.Base: Callback is not null, firing Callback...');
-
-        _this._Callback(_this);
-    } else {
-        console.log('_this.Callback: ', _this._Callback +' is apparently null. Typeof: ', typeof _this._Callback)
+    // Fire callback if it is not null
+    if (_this._callback != null) {
+        _this._callback(_this);
     }
 
-    console.log('Typeof _this._Callback: ', typeof _this._Callback)
-
     Object.defineProperty(_this, '_construct', {
-        enumerable: false
+        enumerable: false,
+        writable: false,
+        configurable: false,
     });
 };
 
 System.Models.Record = function(Options, Callback = null){
     //System.Models.Base.call(this);
-    System.Models.Base.call(this, 
+    System.Models.Base.call(
+        this, 
         {
             ID: null,
             Name: null,
@@ -76,21 +64,8 @@ System.Models.Record = function(Options, Callback = null){
         Options,
         Callback
     );
-
-    //var _this = this;
-
-    // this._construct(
-    //     {
-    //         Id: null,
-    //         Name: null,
-    //         Body: ""
-    //     },
-    //     Options,
-    //     function() {
-    //         _this._____xyz = null;
-    //     }
-    // );
 };
+
 System.Models.Record.prototype = Object.create(System.Models.Base.prototype);
 System.Models.Record.prototype.constructor = System.Models.Record;
 
